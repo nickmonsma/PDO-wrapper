81ca9f06861c5dbda870e4d7fb0aa9fe4ccad68d
Monsma <monsma_habfly@hotmail.nl>
2013-07-02 23:28:54 +0200

    PDO Wrapper

---
+++ class.database.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Class Database - MySQL Handeling - Engine PDO;
+ */
+
+class Database
+{	
+	private $connected, $link, $result, $params = null;
+	
+	public function __Construct($hostname, $username, $password, $database)
+	{
+		if($this->connected)
+		{
+			return;
+		}
+		
+		try
+		{
+			$this->link = new PDO("mysql:dbname={$database};host={$hostname}", $username, $password);
+		}
+		catch(PDOException $exception)
+		{
+			die($exception->getMessage());
+		}
+		
+		$this->connected = true;
+	}
+	
+	public function query($sql)
+	{
+		return $this->prepare($sql)->execute();
+	}
+	
+	public function prepare($sql)
+	{
+		if(!$this->result = $this->link->prepare($sql))
+		{
+			return print_r($this->result->errorInfo());
+		}
+		
+		return $this;
+	}
+	
+	public function bind()
+	{
+		$this->params = func_get_args();
+		
+		return $this;
+	}
+	
+	public function execute()
+	{
+		if(!$this->result->execute($this->params))
+		{
+			return print_r($this->result->errorInfo());
+		}
+		
+		return new PDOResult($this->result, $this->link->lastInsertId());
+	}
+}
+
+class PDOResult
+{
+	public $num_rows, $insert_id;
+	private $result;
+	
+	public function __Construct($result, $insert_id)
+	{
+		$this->result = $result;
+		
+		$this->insert_id = $insert_id;
+		
+		$this->num_rows = $result->rowCount();
+	}
+	
+	public function result()
+	{
+		return $this->result->fetchColumn();
+	}
+	
+	public function fetch_assoc()
+	{
+		return $this->result->fetch(PDO::FETCH_ASSOC);
+	}
+}
+?>
